22b6e04117465bc459e9499c1debb9bd2d12efd6
cfeliot <cristoferfrnd@gmail.com>
2022-02-12 10:12:49 -0500

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Cristofer Pujota on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
